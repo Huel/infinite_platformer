a0f79bb2972ff34b08bb7d89c4664bbed312c1d2
Huel Fuchsberger <huel.fuchsberger@gmx.de>
2018-08-09 22:44:57 +0300

    Lives system

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,11 +6,30 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        [SerializeField]
+        public GameObject playManager;
+        [SerializeField]
+        public GameController  gameController;
+
+        private void Start()
+        {
+            if (!playManager)
+                playManager = GameObject.Find("PlayManager");
+            
+            gameController = playManager.GetComponent(typeof(GameController)) as GameController;
+        }
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+                --gameController.playerLives;
+
+                if (gameController.playerLives <1 )
+                {
+                     Debug.Log("OUT OF LIVES!");
+                }
+                else
+                    gameController.RespawnPlayer();        
             }
         }
     }
