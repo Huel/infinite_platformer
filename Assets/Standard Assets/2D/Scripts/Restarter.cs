91d0049c18fa4027905a6a3150f0e69c5599fc6b
Huel Fuchsberger <huel.fuchsberger@gmx.de>
2018-08-10 21:15:49 +0300

    Killzone follows player Character

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -10,6 +10,10 @@ namespace UnityStandardAssets._2D
         public GameObject playManager;
         [SerializeField]
         public GameController  gameController;
+        [SerializeField]
+        public GameObject  playerCharacter;
+
+        private Transform playerTransform;
 
         private void Start()
         {
@@ -17,7 +21,19 @@ namespace UnityStandardAssets._2D
                 playManager = GameObject.Find("PlayManager");
             
             gameController = playManager.GetComponent(typeof(GameController)) as GameController;
+
+            if (!playerCharacter)
+                playerCharacter = GameObject.Find("CharacterRobotBoy");
+
+            playerTransform = playerCharacter.GetComponent<Transform>();
+
         }
+
+        private void Update() 
+        {
+            this.gameObject.transform.position = new Vector3(playerTransform.position.x,this.gameObject.transform.position.y,this.gameObject.transform.position.z);
+        }
+    
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag == "Player")
