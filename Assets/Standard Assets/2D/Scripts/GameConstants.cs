bfa8b0e1b281143330f1b37ddb2a370a05543825
Huel Fuchsberger <huel.fuchsberger@gmx.de>
2018-08-10 21:49:08 +0300

    Added offset

--- Assets/Standard Assets/2D/Scripts/GameConstants.cs
+++ Assets/Standard Assets/2D/Scripts/GameConstants.cs
@@ -6,4 +6,5 @@ public class GameConstants : ScriptableObject
 {
     public const int PLAYER_LIVES = 5;
     public const float PLAYER_RESPAWN_HIGHT= 10.0f;
+    public const float CAMERA_OFFSET = 2.0f;
 }
--- Assets/Standard Assets/2D/Scripts/GameController.cs
+++ Assets/Standard Assets/2D/Scripts/GameController.cs
@@ -8,24 +8,54 @@ namespace UnityStandardAssets._2D
 	{
 		[SerializeField]
 		public GameObject playerCharacter;
+		private Transform playerCharacterTransform;
+		private PlatformerCharacter2D playerController;
 		[SerializeField]
 		public int playerLives;
 
+		[SerializeField]
+		public GameObject mainCamera;
+
+		private Transform mainCameraTransform;
+
+
 		// Use this for initialization
-		public void RespawnPlayer()
-		{
-			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x,GameConstants.PLAYER_RESPAWN_HIGHT,0.0f);
-			playerCharacter.gameObject.GetComponent<PlatformerCharacter2D>().Move(0.0f,false,false,true);
-		}
 		void Start () 
 		{
+			if(!playerCharacter)
+				playerCharacter = GameObject.Find("CharacterRobotBoy");
+
+			playerCharacterTransform = playerCharacter.GetComponent<Transform>();
+			playerController = playerCharacter.GetComponent<PlatformerCharacter2D>();
+			
+			if(!mainCamera)
+				playerCharacter = GameObject.Find("MainCamera");
+
+			 mainCameraTransform = playerCharacter.GetComponent<Transform>();
+
 			playerLives = GameConstants.PLAYER_LIVES;
 		}
 		
 		// Update is called once per frame
 		void Update () 
-		{
+		{		
+			float newPositionX = playerCharacterTransform.position.x;
+
+			if(playerController.GetFacingRight())
+			{
+				newPositionX += GameConstants.CAMERA_OFFSET;
+			}
+			else
+				newPositionX -=GameConstants.CAMERA_OFFSET;
+				
 			
+
+			mainCameraTransform.position = new Vector3(newPositionX,mainCameraTransform.gameObject.transform.position.y,mainCameraTransform.gameObject.transform.position.z);
+		}
+		public void RespawnPlayer()
+		{
+			playerCharacter.transform.position = new Vector3 (playerCharacter.transform.position.x,GameConstants.PLAYER_RESPAWN_HIGHT,0.0f);
+			playerCharacter.gameObject.GetComponent<PlatformerCharacter2D>().Move(0.0f,false,false,true);
 		}
 	}
 }
